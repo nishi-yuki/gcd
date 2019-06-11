f1c9222bf66fd8d9453e6e592ee076ff69579dcd
NishiYuki <nishiyk256@gmail.com>
2019-06-12 00:33:12 +0900

    add: "(back)" choice

--- functions/gcd.fish
+++ functions/gcd.fish
@@ -1,8 +1,12 @@
 function gcd -d "Change the current directory to repository's directory"
-    set -l repo_path (ghq list -p | peco)
+    set -l backmsg "(back)"
+    set -l ghq_root (ghq root)
+    set -l repo_path "$ghq_root"/(string split " " "$backmsg" (ghq list) | peco)
     switch "$repo_path"
         case ""
             return 1
+        case "$ghq_root"/"$backmsg"
+            return 0
         case "*"
             cd "$repo_path"
         end
